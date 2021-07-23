ad606959169a4e40056a196fac9d3d13795597ea
203406 <78428042+203406@users.noreply.github.com>
2021-07-23 12:49:20 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+
+
+function login() {
+  const user = document.getElementById("user").value;
+  const password = document.getElementById("password").value;
+  if (user == "daniel" && password == "12345") {
+   // alert("Bienvenido Daniel");
+    location.href='./segundaVista.html';
+  } else {
+    alert("Datos erroneos");
+  }
+}
+
+function sendParams() {
+  const host = document.getElementById("host").value;
+  const user = document.getElementById("user").value;
+  const password = document.getElementById("password").value;
+  const database = document.getElementById("database").value;
+
+  document.getElementById(
+"txtData").value = `host: ${host} user: ${user} password: ${password} database: ${database}`;
+}
+
+
+function addData() {
+  // Crear query para INSERT, SELECT, UPDATE O DELETE
+  const nombre = document.getElementById("nombre").value;
+  const ap_pat = document.getElementById("ap_pat").value;
+  const ap_mat = document.getElementById("ap_mat").value;
+  const edad = document.getElementById("edad").value;
+
+  $query = `INSERT INTO persona (nombre, ap_pat, ap_mat, edad) VALUES (${nombre},${ap_pat},${ap_mat},${edad} )`;
+
+  con.query($query, function (err, rows, fields) {
+    if (err) {
+      console.log("Error Query");
+      console.log(err);
+      return;
+    }
+
+    console.log("Query exitoso", rows);
+  });
+
+  con.end(function () {
+    // Conexión Finalizada
+  });
+  // Input data conection database
+}
